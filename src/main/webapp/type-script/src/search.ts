9ce46eef4f22e0312fb1b6cc79fdcbaa6037084a
Aria Haghighi <me@aria42.com>
2015-07-26 20:13:38 -0700

    Separate Typescript compiled JS

--- src/main/webapp/type-script/src/search.ts
+++ src/main/webapp/type-script/src/search.ts
@@ -1,15 +1,23 @@
-var $searchBtn = $('#search-submit')
 var $searchField = $('#search-field')
+var $searchResults = $('#search-results')
 
 function apiCall(query: string) {
   var searchURI = '/api/0.1/search/' + encodeURIComponent(query)
   $.getJSON(searchURI, function(data) {
-      
+    $searchResults.empty()
+    var $list = $('<ul></ul>')
+    for (result: data) {
+
+    }
+    $searchResults.append($list)
   })
 }
 
-$searchBtn.on('click', function(e) {
+$searchField.keydown(function(e) {
+  var code = e.keyCode || e.which
+  if (code != 13) {
+    return true
+  }
   var text = $searchField.val()
   apiCall(text)
-  console.info('search-clicked ' + text)
 })
