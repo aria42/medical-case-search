33a291b84a78b661967204feeb6213cc8a6f5bb5
Aria Haghighi <me@aria42.com>
2015-07-26 21:05:23 -0700

    Use sys.env for environmental var access

--- src/main/scala/JettyLauncher.scala
+++ src/main/scala/JettyLauncher.scala
@@ -4,15 +4,15 @@ import org.eclipse.jetty.webapp.WebAppContext
 
 object JettyLauncher { // this is my entry object as specified in sbt project definition
   def main(args: Array[String]) {
-    val port = if(System.getenv("PORT") != null) System.getenv("PORT").toInt else 8081
-
+    val port = sys.env.get("PORT").map(_.toInt).getOrElse(8081)
+    // Setup context
     val context = new WebAppContext()
     context.setContextPath("/")
     context.setResourceBase("src/main/webapp/WEB-INF")
     context.addServlet(classOf[CaseSearchAPIServlet], "/api/0.1/*")
     context.addServlet(classOf[WebappServlet], "/app/*")
     context.setParentLoaderPriority(true)
-
+    // Start server and join
     val server = new Server(port)
     server.setHandler(context)
     server.start
